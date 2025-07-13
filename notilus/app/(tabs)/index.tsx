eb103498b7a72ec938337be1081877dfb0372023
NicolasJouan <nicolasjouan7@gmail.com>
2025-07-13 23:40:58 +0200

    changes have been made

--- notilus/app/(tabs)/index.tsx
+++ notilus/app/(tabs)/index.tsx
@@ -6,9 +6,32 @@ import * as SQLite from 'expo-sqlite'
 
 export default function Index() {
   const [text, setText] = useState('')
-  
+  const [title, setTitle] = useState('') 
+
   //import from the database the note saved on opening the page
-  useEffect(() => { setText('Hi serpentus')}, [])
+  
+  const createNote = async () => {
+    const db = await SQLite.openDatabaseAsync('noteDB');
+    await db.execAsync(`
+    CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    content TEXT,
+    updatedAt TEXT
+    );
+    INSERT INTO notes (title, content, updatedAt) VALUES ('Title 1', 'Hi venomous how are you', '12:00')
+    `)
+    const result = await db.getFirstAsync('SELECT * FROM notes') 
+    console.log(result)
+    setText(String(result.content))
+    setTitle(String(result.title))
+  };
+
+  useEffect(() => {
+    createNote()
+    // setText(result.content);
+  }, [])
+  
 
 
 
@@ -20,6 +43,7 @@ export default function Index() {
       <View>
         <Link href={"/(tabs)/to-do"}>To Do</Link>
       </View>
+      <TextInput value={title} onChangeText={setTitle} placeholder="Title here..."></TextInput>
       <TextInput value={text} onChangeText={setText} placeholder="Write here..."></TextInput>
     </View>
   );
